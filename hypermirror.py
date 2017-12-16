3d736e0f638f4c0e8e6b83b16592c9cc5cbd9d19
Hilton De Meillon <hdemeillon@telenet.com.au>
2017-12-16 20:27:43 +0800

    fixes logic error and changes output to use Printer function

--- hypermirror.py
+++ hypermirror.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
+""" This script starts a hyper.sh container on Yubikey
+   insertion and stops it on removal """
 
+import sys
 import time
 import subprocess
 import osquery
@@ -16,8 +19,17 @@ def search():
     # search for a Yubikey
     for device in usb_devices.response:
         for usb_attributes in device:
-            if usb_devices.response[usb_devices.response.index(device)]['vendor'] == "Yubico":
+            if usb_devices.response[usb_devices.response.index(device)]['vendor'] == "Yubico" and usb_devices.response[usb_devices.response.index(device)]['model'] == "Yubikey NEO OTP+U2F+CCID":
                 return True
+    return False
+
+class Printer():
+    """
+    Print things to stdout on one line dynamically
+    """
+    def __init__(self, data):
+        sys.stdout.write("\r\x1b[K"+data.__str__())
+        sys.stdout.flush()
 
 def container_running():
     """This function checks is a hyper container is running"""
@@ -28,7 +40,8 @@ if __name__ == "__main__":
     while True:
         cont_status = container_running()
         usb_status = search()
-        print ("Container running : %s Yubikey inserted : %s") % (cont_status, usb_status)
+        screen_output =  "Container running : %s Yubikey inserted : %s" % (cont_status, usb_status)
+        Printer(screen_output)
         time.sleep(5)
         if cont_status == False and usb_status == True:
             time.sleep(3)
@@ -40,7 +53,7 @@ if __name__ == "__main__":
             time.sleep(3)
             print "Attaching FIP"
             subprocess.call(['/usr/local/bin/hyper', 'fip', 'attach', 'access', 'boring-hopper'])
-        elif usb_status != True and cont_status == True:
+        elif usb_status == False and cont_status == True:
             print "detaching FIP"
             time.sleep(3)
             subprocess.call(['/usr/local/bin/hyper', 'fip', 'detach', 'boring-hopper'])
