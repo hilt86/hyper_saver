dfc3c1a6dc59fabc8ee60d7ba4326108f649c7fc
Hilton De Meillon <hdemeillon@telenet.com.au>
2017-12-16 16:41:04 +0800

    adds functionality to stop and start hyper containers based on device insertion

--- hypermirror.py
+++ hypermirror.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
+import time
 import osquery
+import subprocess
 
 def search():
 
@@ -20,5 +22,20 @@ def search():
 if __name__ == "__main__":
     if search():
         print "a Yubikey is plugged in"
+        time.sleep(3)
+        print "Starting Boring Hopper"
+        subprocess.call(['/usr/local/bin/hyper', 'start', 'boring-hopper'])
+        time.sleep(3)
+        print "Starting sshd"
+        subprocess.call(['/usr/local/bin/hyper', 'exec', '-d', 'boring-hopper', '/usr/sbin/sshd'])
+        time.sleep(3)
+        print "Attaching FIP"
+        subprocess.call(['/usr/local/bin/hyper', 'fip', 'attach', 'access', 'boring-hopper'])
     else:
         print "no Yubikey device is plugged in"
+        print "detaching FIP"
+        time.sleep(3)
+        subprocess.call(['/usr/local/bin/hyper', 'fip', 'detach', 'boring-hopper'])
+        time.sleep(3)
+        print "Stopping Boring Hopper"
+        subprocess.call(['/usr/local/bin/hyper', 'stop', 'boring-hopper'])
